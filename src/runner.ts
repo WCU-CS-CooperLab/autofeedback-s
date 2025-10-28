f8e98b7ae0ba70df528d091f40771679bc2867d6
David Cooper <dcooper@wcupa.edu>
2025-10-27 22:13:32 -0400

    fix linter

--- src/runner.ts
+++ src/runner.ts
@@ -443,7 +443,7 @@ export const runAll = async (tests: Array<Test>, cwd: string): Promise<void> => 
       let notice = `🏁 Passed ${test.name}\n`
       notice += '\n' + result + '\n'
       //core.notice(notice, nAnn)
-      await setCheckRunOutput(notice.substring(0,maxChars), test.name)
+      await setCheckRunOutput(notice.substring(0, maxChars), test.name)
 
       if (test.points) {
         points += test.points
