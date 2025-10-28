57c45e4c134ba448c84ccdd898ebabedfcbe4ac0
David Cooper <dcooper@wcupa.edu>
2025-10-27 23:03:50 -0400

    Update Hello.java

--- src/__tests__/java/Hello.java
+++ src/__tests__/java/Hello.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Hello {
     public static void main(String[] args) {
         String line;
         Scanner scanner = new Scanner(System.in);
-
+        File file = new File("input.txt);
+        
         System.out.println("What is your name?");
         line = scanner.nextLine();
         System.out.println("Hello " + line);
+
+        try{
+            Scanner fileScanner = new Scanner(file);
+            while(fileScanner.hasNextLine()) {
+                System.out.println(file.nextLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
